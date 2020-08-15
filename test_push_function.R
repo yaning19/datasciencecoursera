49b596499aeb57f842c4ab61da44b0f2fc7ea414
Yaning Zhang <68801463+yaning19@users.noreply.github.com>
2020-08-15 11:05:14 -0700

    Update test_push_function.R

--- test_push_function.R
+++ test_push_function.R
@@ -1,1 +1,13 @@
 ##this is a file to test the "push" function in Rstudio
+##I created a new folder (wd) for this project
+##I "cloned" this file by creating a new project in Rstudio and choose "vision control"
+##Enter the web link, project name and the new wd path in the pop up window in Rtudio
+#now I see the existing files and can edit this project in my Rstudio
+##I added a new script and wrote the following line:
+##this is a file to test "push" function in Rtusio
+#then I check the checkbox at upper right coner in front of my new file
+##then "commit" my new file
+##doing so, my new file is no longer showing on the upper right coner
+##I then clicked "push" button
+##now I come back to my Github, the file is successfully uploaded here
+##I now am going to test if these text I just wrote will be updated in Rtudio
