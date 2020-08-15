63f345e92d590a6392ebf34a2744d567c85b2243
Yaning Zhang <68801463+yaning19@users.noreply.github.com>
2020-08-15 11:11:37 -0700

    Update and rename test_push_function.R to push_and_pull_in_Rstudio.R
    update (pull) the changes to Rstudio

--- push_and_pull_in_Rstudio.R
+++ push_and_pull_in_Rstudio.R
@@ -1,4 +1,5 @@
 ##this is a file to test the "push" function in Rstudio
+------------------------------------
 ##I created a new folder (wd) for this project
 ##I "cloned" this file by creating a new project in Rstudio and choose "vision control"
 ##Enter the web link, project name and the new wd path in the pop up window in Rtudio
@@ -11,3 +12,9 @@
 ##I then clicked "push" button
 ##now I come back to my Github, the file is successfully uploaded here
 ##I now am going to test if these text I just wrote will be updated in Rtudio
+
+-------------------------------------
+##I used "pull" button in Rtudio and can see all the new text I did in Github in my Rstudio
+##I also checked "history" of my commits and see all the changes I made in the last 10 mins
+##this file will be used for my future reference if I ever need to use Rtudio to edit projects I obtained from Github
+-------------------------------------
